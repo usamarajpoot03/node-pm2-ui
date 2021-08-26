5c931d8eefdcb3bfee6a24a26adb18bf68b1c231
usamarajpoot03 <usamarajpoot03@yahoo.com>
2021-08-27 01:56:06 +0500

    code pushed

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    apps : [{
+      name   : "api",
+      script : "./src/server.js"
+    }]
+  }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const fs = require('fs')
+
 const path = require('path');
 
 var app = express();
 const cors = require("cors");
 app.use(cors());
 const errorHandler = require("./routes/middlewares/errorHandler");
+const { reject } = require("lodash");
 
 app.use(express.json());
 
@@ -13,17 +16,39 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 
+app.get('/', function(req,res) {
+  res.render("index", { title: "Home" });
+});
+
+app.get('/logout', function(req,res) {
+  res.render("index", { title: "Home" });
+});
+
 app.get("/user", (req, res) => {
   res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
-app.use('/', function(req,res) {
-  res.render("index", { title: "Home" });
+app.get("/logs",async (req, res) => {
+  const fileContent  =  await readTextFile(  path.resolve(__dirname , '../errorLogFile.log'));
+  res.render("logs", { title: "Profile", logs: fileContent });
 });
 
 
+app.get("/api/test",   require('./routes/api/demo.controller'))
 
-app.use("/api/test",   require('./routes/api/demo.controller'))
+async function readTextFile(filepath)
+{
+  return new Promise((resolve, reject)=>{
+  fs.readFile(filepath, 'utf8' , (err, data) => {
+    if (err) {
+     reject(err);
+    }
+    resolve(data);
+  })
+})
+
+}
+
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 8000 ;
